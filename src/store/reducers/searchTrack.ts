baa2a3652af98e84e059c7197280ed254b762029
chiefnaheem <naheemademola21@gmail.com>
2023-09-03 15:16:34 -0700

    updates

--- src/store/reducers/searchTrack.ts
+++ src/store/reducers/searchTrack.ts
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "..";
-
+const BASE_URL = "https://t2x9d5qiii.execute-api.us-east-1.amazonaws.com/dev/";
 
 
 export const searchTrack = createAsyncThunk(
     "remoteApp/searchTrack",
     async (trackName: string, thunkAPI) => {
         const { data } = await axios.get(
-            `search?query=${trackName ? trackName : "a"}&type=track`
+            `${BASE_URL}search?query=${trackName ? trackName : "a"}&type=track`
         );
         // console.log('data')
         // console.log(data.data);
@@ -20,7 +20,7 @@ export const getArtist = createAsyncThunk(
     "remoteApp/getArtist",
     async (artistId: number, thunkAPI) => {
         const { data } = await axios.get(
-            `artist/${artistId}`
+            `${BASE_URL}artist/${artistId}`
         );
         console.log('data22')
         // console.log(data.data);
