6c7a78349d9626a1018acb613f63ce1a9e48ee1b
chiefnaheem <naheemademola21@gmail.com>
2023-09-03 15:31:01 -0700

    increased limit

--- src/store/reducers/searchTrack.ts
+++ src/store/reducers/searchTrack.ts
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "..";
-const BASE_URL = "https://t2x9d5qiii.execute-api.us-east-1.amazonaws.com/dev/";
+
 
 
 export const searchTrack = createAsyncThunk(
     "remoteApp/searchTrack",
     async (trackName: string, thunkAPI) => {
         const { data } = await axios.get(
-            `${BASE_URL}search?query=${trackName ? trackName : "a"}&type=track`
+            `search?query=${trackName ? trackName : "a"}&type=track&limit=10000`
         );
         // console.log('data')
         // console.log(data.data);
@@ -20,7 +20,7 @@ export const getArtist = createAsyncThunk(
     "remoteApp/getArtist",
     async (artistId: number, thunkAPI) => {
         const { data } = await axios.get(
-            `${BASE_URL}artist/${artistId}`
+            `artist/${artistId}`
         );
         console.log('data22')
         // console.log(data.data);
