7f4c790a304e1a4a19a5b8f2c57c2a54f93538cf
chiefnaheem <naheemademola21@gmail.com>
2023-09-04 02:58:01 -0700

    fix

--- src/store/reducers/searchTrack.ts
+++ src/store/reducers/searchTrack.ts
@@ -2,13 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "..";
 
-
+const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const targetApiUrl = 'https://t2x9d5qiii.execute-api.us-east-1.amazonaws.com/dev';
 
 export const searchTrack = createAsyncThunk(
     "remoteApp/searchTrack",
     async (trackName: string, thunkAPI) => {
         const { data } = await axios.get(
-            `search?query=${trackName ? trackName : "a"}&type=track&limit=10000`
+            `${corsProxyUrl}${targetApiUrl}/search?query=${trackName ? trackName : "a"}&type=track&limit=10000`
         );
         // console.log('data')
         // console.log(data.data);
@@ -20,7 +21,7 @@ export const getArtist = createAsyncThunk(
     "remoteApp/getArtist",
     async (artistId: number, thunkAPI) => {
         const { data } = await axios.get(
-            `artist/${artistId}`
+            `${corsProxyUrl}${targetApiUrl}/artist/${artistId}`
         );
         console.log('data22')
         // console.log(data.data);
